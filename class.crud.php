7638a005d92fa3af27d73b95fbb1329fcb8e5681
gugu79 <gustavo.lucas@vecell.com.br>
2016-03-19 16:02:42 -0300

    class.crud.php
    Add UpdateAssociados, UpdateConveniadas, Select TOP 10 ao invés de NOT
    IN (10)

--- class.crud.php
+++ class.crud.php
@@ -53,16 +53,9 @@ class Crud {
 													IsNull(Desconto, Cast('0' as Bit)) As Desconto, 
 													IsNull(OptInNews, Cast('0' as Bit)) As OptInNews, 
 													IsNull(Invisivel_Btn_Ass, Cast('0' as Bit)) As Invisivel_Btn_Ass
-											  FROM 	TB_Associacoes 
-											 ORDER BY Sigla ASC" ); 
-
-/*											SELECT 	PK_Codigo, 
-									 				Nome, 
-													Sigla,
-													CNPJ,
-													IsNull(Invisivel_Btn_Ass, Cast('0' as Bit)) As Oculto
-											  FROM 	TB_Associacoes */
-											  
+											FROM 	TB_Associacoes 
+											ORDER BY Sigla ASC" ); 
+										  
 			if($oResult)
 				{
 					//percorre os resultados via o fetch()
@@ -216,7 +209,8 @@ class Crud {
 	// Função para listar associaçoes
 	function getAssociados() {
 		try {
-			$oResult = $this->oCon->query(" SELECT 	PK_Matricula, FK_Assoc, 
+			$oResult = $this->oCon->query(" SELECT 	TOP 10 
+													PK_Matricula, FK_Assoc, 
 			                                        Nome, Lim_Credito, Bloq, Motivo,
 			                                        Endereco, Bairro, CEP, Cidade, FK_UF,
 													Fone, Celular, Email, Email2, OptInNews, 
@@ -233,8 +227,8 @@ class Crud {
 													Dt_Plano_Sta_Casa, Plano_Santa_Casa,
 													Dt_Angelus, Angelus,
 													Dt_Clube_Desc, Clube_Descontos
-											  FROM 	TB_Clientes 
-											  WHERE FK_Assoc NOT IN (10)"); 
+											FROM 	TB_Clientes
+											ORDER BY Nome ASC"); 
 
 											  
 			if($oResult)
@@ -249,31 +243,123 @@ class Crud {
 
 		return $oAssociados;
 	}
+
+
+	// Função para atualizar associações
+	function updateAssociados($aUpdateAssociados) {
+		try {
+			$oUpdateAssociados = $this->oCon->prepare(" UPDATE TB_Clientes 
+															SET PK_Matricula		= :PK_Matricula, 
+																FK_Assoc			= :FK_Assoc,
+																Nome				= :Nome, 
+																Lim_Credito			= Cast(:Lim_Credito as Money), 
+																Bloq				= Cast(:Bloq as bit), 
+																OptInNews			= Cast(:OptInNews as bit), 
+																Email				= :Email, 
+																Endereco			= :Endereco, 
+																Fone				= :Fone,
+																Celular				= :Celular, 
+																CPF					= :CPF, 
+																Bairro				= :Bairro,
+																Cep					= :Cep, 
+																Cidade				= :Cidade, 
+																FK_UF				= :FK_UF, 
+																RG					= :RG,
+																Bco					= :Bco, 
+																Ag					= :Ag, 
+																Cta					= :Cta, 
+																Motivo				= :Motivo,
+																Seguro				= Cast(:Seguro as Money), 
+																Mensalidade			= Cast(:Mensalidade as Money),
+																Dt_Cadastro			= Cast(:Dt_Cadastro as Datetime), 
+																Dt_Nasc				= Cast(:Dt_Nasc as Datetime), 
+																Plano_Santa_Casa	= Cast(:Plano_Santa_Casa as Money), 
+																Angelus				= Cast(:Angelus as Money), 
+																Assist_Juridica		= Cast(:Assist_Juridica as Money), 
+																Extrato				= Cast(:Extrato as Money), 
+																Colombo_Virtual		= Cast(:Colombo_Virtual as Money), 
+																Clube_Descontos		= Cast(:Clube_Descontos as Money), 
+																Mensal_Cartao		= Cast(:Mensal_Cartao as Money), 
+																Nome_Seguro			= :Nome_Seguro, 
+																Nome_Ass_Jur		= :Nome_Ass_Jur,
+																Dt_Mensalidade		= Cast(:Dt_Mensalidade as Datetime), 
+																Dt_Mensal_Cartao	= Cast(:Dt_Mensal_Cartao as Datetime), 
+																Dt_Seguro			= Cast(:Dt_Seguro as Datetime), 
+																Dt_Assist_Jurid		= Cast(:Dt_Assist_Jurid as Datetime), 
+																Dt_Colombo_Virt		= Cast(:Dt_Colombo_Virt as Datetime), 
+																Dt_Extrato			= Cast(:Dt_Extrato as Datetime), 
+																Dt_Plano_Sta_Casa	= Cast(:Dt_Plano_Sta_Casa as Datetime), 
+																Dt_Angelus			= Cast(:Dt_Angelus as Datetime), 
+																Dt_Clube_Desc		= Cast(:Dt_Clube_Desc as Datetime), 
+																Email2				= :Email2, 
+																Cargo				= :Cargo, 
+																Secretaria			= :Secretaria 
+													From TB_Clientes Where (PK_Matricula=".cod_mat.") And (FK_Assoc=".p_codigo.");"); 
+			$oUpdateAssociados->bindParam(":Nome", $aUpdateAssociados['Nome'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":PK_Matricula", $aUpdateAssociados['PK_Matricula'] , PDO::PARAM_INT);
+			$oUpdateAssociados->bindParam(":FK_Assoc", $aUpdateAssociados['FK_Assoc'] , PDO::PARAM_INT);
+			$oUpdateAssociados->bindParam(":Lim_Credito", $aUpdateAssociados['Lim_Credito'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Bloq", $aUpdateAssociados['Bloq'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":RG", $aUpdateAssociados['RG'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Email", $aUpdateAssociados['Email'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Endereco", $aUpdateAssociados['Endereco'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Fone", $aUpdateAssociados['Fone'] , PDO::PARAM_INT);
+			$oUpdateAssociados->bindParam(":Celular", $aUpdateAssociados['Celular'] , PDO::PARAM_INT);
+			$oUpdateAssociados->bindParam(":CPF", $aUpdateAssociados['CPF'] , PDO::PARAM_INT);
+			$oUpdateAssociados->bindParam(":Bairro", $aUpdateAssociados['Bairro'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Cep", $aUpdateAssociados['Cep'] , PDO::PARAM_INT);
+			$oUpdateAssociados->bindParam(":Cidade", $aUpdateAssociados['Cidade'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":FK_UF", $aUpdateAssociados['FK_UF'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Bco", $aUpdateAssociados['Bco'] , PDO::PARAM_INT);
+			$oUpdateAssociados->bindParam(":Ag", $aUpdateAssociados['Ag'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Cta", $aUpdateAssociados['Cta'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Motivo", $aUpdateAssociados['Motivo'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":OptInNews", $aUpdateAssociados['OptInNews'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Seguro", $aUpdateAssociados['Seguro'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Mensalidade", $aUpdateAssociados['Mensalidade'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Mensal_Cartao", $aUpdateAssociados['Mensal_Cartao'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Dt_Nasc", $aUpdateAssociados['Dt_Nasc'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Plano_Santa_Casa", $aUpdateAssociados['Plano_Santa_Casa'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Assist_Juridica", $aUpdateAssociados['Assist_Juridica'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Angelus", $aUpdateAssociados['Angelus'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Extrato", $aUpdateAssociados['Extrato'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Colombo_Virtual", $aUpdateAssociados['Colombo_Virtual'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Clube_Descontos", $aUpdateAssociados['Clube_Descontos'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Dt_Mensalidade", $aUpdateAssociados['Dt_Mensalidade'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Dt_Mensal_Cartao", $aUpdateAssociados['Dt_Mensal_Cartao'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Dt_Seguro", $aUpdateAssociados['Dt_Seguro'] , PDO::PARAM_STR);
+			$oUpdateAssociados->bindParam(":Dt_Assist_Jurid", $aUpdateAssociados['Dt_Assist_Jurid'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Dt_Colombo_Virt", $aUpdateAssociados['Dt_Colombo_Virt'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Dt_Extrato", $aUpdateAssociados['Dt_Extrato'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Dt_Plano_Sta_Casa", $aUpdateAssociados['Dt_Plano_Sta_Casa'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Dt_Angelus", $aUpdateAssociados['Dt_Angelus'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Dt_Clube_Desc", $aUpdateAssociados['Dt_Clube_Desc'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Nome_Seguro", $aUpdateAssociados['Nome_Seguro'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Nome_Ass_Jur", $aUpdateAssociados['Nome_Ass_Jur'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Email2", $aUpdateAssociados['Email2'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Cargo", $aUpdateAssociados['Cargo'] , PDO::PARAM_STR);
+ 			$oUpdateAssociados->bindParam(":Secretaria", $aUpdateAssociados['Secretaria'] , PDO::PARAM_STR);
+
+			$oUpdateAssociados->execute();
+		} catch(PDOException $e) {
+	    	echo 'ERROR: ' . $e->getMessage();
+		}
+
+		return true;
+	}
+	
 	
 	
 	
 	// Função para listar associaçoes
 	function getConveniadas() {
 		try {
-			$oResult = $this->oCon->query(" SELECT 	PK_Matricula, FK_Assoc, 
-			                                        Nome, Lim_Credito, Bloq, Motivo,
-			                                        Endereco, Bairro, CEP, Cidade, FK_UF,
-													Fone, Celular, Email, Email2, OptInNews, 
-													Cargo, Secretaria, 
-													RG, CPF, Bco, Ag, Cta,
-													Rescisao, Dt_Rescisao, 
-													Dt_Cadastro, Dt_Nasc,
-													Dt_Mensalidade, Mensalidade, 
-													Dt_Mensal_Cartao, Mensal_Cartao, 
-													Dt_Seguro, Seguro, Nome_Seguro, 
-													Dt_Assist_Jurid, Assist_Juridica, Nome_Ass_Jur, 
-													Dt_Colombo_Virt, Colombo_Virtual,
-													Dt_Extrato, Extrato,
-													Dt_Plano_Sta_Casa, Plano_Santa_Casa,
-													Dt_Angelus, Angelus,
-													Dt_Clube_Desc, Clube_Descontos
-											  FROM 	TB_Clientes 
-											  WHERE FK_Assoc NOT IN (10)"); 
+			$oResult = $this->oCon->query(" SELECT 	TOP 10 
+													PK_Codigo, 
+													Nome AS Nome, 
+													IsNull(Logotipo, '') AS Logotipo
+											FROM TB_Conveniadas 
+											ORDER BY TB_Conveniadas.Nome ASC "); 
 
 											  
 			if($oResult)
@@ -292,16 +378,137 @@ class Crud {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	function updateConveniadas($aUpdateConveniadas) {
+		try {
+			$oUpdateConveniadas = $this->oCon->prepare(" UPDATE TB_Conveniadas 
+															SET Razao					= :Razao, 
+																Nome					= :Nome, 
+																CNPJ					= :CNPJ, 
+																Insc_Est				= :Insc_Est, 
+																Email					= :Email, 
+																Site					= :Site, 
+																Bco						= :Bco, 
+																Ag						= :Ag, 
+																Cta						= :Cta, 
+																Bco2					= :Bco2,
+																Ag2						= :Ag2, 
+																Cta2					= :Cta2, 
+																Endereco				= :Endereco, 
+																Bairro					= :Bairro, 
+																Cep						= :Cep, 
+																Cidade					= :Cidade, 
+																FK_UF					= :FK_UF, 
+																Fone					= :Fone, 
+																Fax						= :Fax, 
+																Celular					= :Celular, 
+																FK_Segmento				= :FK_Segmento, 
+																Condicoes				= :Condicoes, 
+																Bloq					= Cast(:Bloq as bit), 
+																Motivo					= :Motivo, 
+																OptInNews				= Cast(:OptInNews as bit), 																Alt=Cast('0' as Bit), 
+																FK_Grupo				= :FK_Grupo, 
+																Contato					= :Contato, 
+																Fone_Contato			= :Fone_Contato, 
+																Dt_Cadastro				= Cast(:Dt_Cadastro as Datetime), 
+																Tx_Adm_CDC				= Cast(:Tx_Adm_CDC as Numeric(4,3)), 
+																Loja_Assoc				= Cast(:Loja_Assoc as bit), 
+																Pessoa					= :Pessoa, 
+																Nro_Contrato			= :Nro_Contrato, 
+																Data_Contrato			= Cast(:Data_Contrato as Datetime), 
+																Consultor				= :Consultor, 
+																Valor_Contrato			= Cast(:Valor_Contrato as Money), 
+																Anuidade_Contrato		= Cast(:Anuidade_Contrato as Money),
+																Forma_Pgto				= :Forma_Pgto, 
+																Data_Pgto1				= Cast(:Data_Pgto1 as Datetime), 
+																Valor_Pgto1				= Cast(:Valor_Pgto1 as Money), 
+																Situacao_Pg1			= :Situacao_Pg1, 
+																Data_Pgto2				= Cast(:Data_Pgto2 as Datetime), 
+																Valor_Pgto2				= Cast(:Valor_Pgto2 as Money), 
+																Situacao_Pg2			= :Situacao_Pg2, 
+																Data_Pgto3				= Cast(:Data_Pgto3 as Datetime), 
+																Valor_Pgto3				= Cast(:Valor_Pgto3 as Money),
+																Situacao_Pg3			= :Situacao_Pg3, 
+																TEF						= :TEF, 
+																TEF_Observacao			= :TEF_Observacao,
+																POS_Modelo				= :POS_Modelo, 
+																POS_Serial				= :POS_Serial, 
+																POS_Data_Inst			= Cast(:POS_Data_Inst as Datetime), 
+																TEF_Data_Inst			= Cast(:TEF_Data_Inst as Datetime), 
+																POS_Valor				= Cast(:POS_Valor as Money), 
+																POS_Mensal				= Cast(:POS_Mensal as Money), 
+																POS_Condicao			= :POS_Condicao, 
+																POS_Data_Mensal1		= Cast(:POS_Data_Mensal1 as Datetime), 
+																Latitude				= :Latitude, 
+																Longitude				= :Longitude
+														FROM TB_Conveniadas 
+														WHERE (PK_Codigo = :PK_Codigo) "); 
+ 			$oUpdateConveniadas->bindParam(":PK_Codigo", $aUpdateConveniadas['PK_Codigo'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Razao", $aUpdateConveniadas['Razao'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Nome", $aUpdateConveniadas['Nome'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":CNPJ", $aUpdateConveniadas['CNPJ'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Insc_Est", $aUpdateConveniadas['Insc_Est'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Email", $aUpdateConveniadas['Email'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Site", $aUpdateConveniadas['Site'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Bco", $aUpdateConveniadas['Bco'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Ag", $aUpdateConveniadas['Ag'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Cta", $aUpdateConveniadas['Cta'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Bco2", $aUpdateConveniadas['Bco2'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Ag2", $aUpdateConveniadas['Ag2'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Cta2", $aUpdateConveniadas['Cta2'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Endereco", $aUpdateConveniadas['Endereco'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Bairro", $aUpdateConveniadas['Bairro'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Cep", $aUpdateConveniadas['Cep'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Cidade", $aUpdateConveniadas['Cidade'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":FK_UF", $aUpdateConveniadas['FK_UF'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Fone", $aUpdateConveniadas['Fone'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Fax", $aUpdateConveniadas['Fax'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Celular", $aUpdateConveniadas['Celular'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":FK_Segmento", $aUpdateConveniadas['FK_Segmento'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Condicoes", $aUpdateConveniadas['Condicoes'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Bloq", $aUpdateConveniadas['Bloq'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Motivo", $aUpdateConveniadas['Motivo'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":OptInNews", $aUpdateConveniadas['OptInNews'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":FK_Grupo", $aUpdateConveniadas['FK_Grupo'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Contato", $aUpdateConveniadas['Contato'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Fone_Contato", $aUpdateConveniadas['Fone_Contato'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Dt_Cadastro", $aUpdateConveniadas['Dt_Cadastro'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Tx_Adm_CDC", $aUpdateConveniadas['Tx_Adm_CDC'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Loja_Assoc", $aUpdateConveniadas['Loja_Assoc'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Pessoa", $aUpdateConveniadas['Pessoa'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Nro_Contrato", $aUpdateConveniadas['Nro_Contrato'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Data_Contrato", $aUpdateConveniadas['Data_Contrato'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Consultor", $aUpdateConveniadas['Consultor'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Valor_Contrato", $aUpdateConveniadas['Valor_Contrato'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Anuidade_Contrato", $aUpdateConveniadas['Anuidade_Contrato'] , PDO::PARAM_STR);
+			$oUpdateConveniadas->bindParam(":Forma_Pgto", $aUpdateConveniadas['Forma_Pgto'] , PDO::PARAM_INT);
+ 			$oUpdateConveniadas->bindParam(":Data_Pgto1", $aUpdateConveniadas['Data_Pgto1'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Valor_Pgto1", $aUpdateConveniadas['Valor_Pgto1'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Situacao_Pg1", $aUpdateConveniadas['Situacao_Pg1'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Data_Pgto2", $aUpdateConveniadas['Data_Pgto2'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Valor_Pgto2", $aUpdateConveniadas['Valor_Pgto2'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Situacao_Pg2", $aUpdateConveniadas['Situacao_Pg2'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Data_Pgto3", $aUpdateConveniadas['Data_Pgto3'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Valor_Pgto3", $aUpdateConveniadas['Valor_Pgto3'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Situacao_Pg3", $aUpdateConveniadas['Situacao_Pg3'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":TEF", $aUpdateConveniadas['TEF'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":TEF_Observacao", $aUpdateConveniadas['TEF_Observacao'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":POS_Modelo", $aUpdateConveniadas['POS_Modelo'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":POS_Serial", $aUpdateConveniadas['POS_Serial'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":POS_Data_Inst", $aUpdateConveniadas['POS_Data_Inst'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":TEF_Data_Inst", $aUpdateConveniadas['TEF_Data_Inst'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":POS_Valor", $aUpdateConveniadas['POS_Valor'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":POS_Mensal", $aUpdateConveniadas['POS_Mensal'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":POS_Condicao", $aUpdateConveniadas['POS_Condicao'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":POS_Data_Mensal1", $aUpdateConveniadas['POS_Data_Mensal1'] , PDO::PARAM_STR);
+ 			$oUpdateConveniadas->bindParam(":Latitude", $aUpdateConveniadas['Latitude'] , PDO::PARAM_STR);
+			$oUpdateConveniadas->bindParam(":Longitude", $aUpdateConveniadas['Longitude'] , PDO::PARAM_STR);
+			$oUpdateConveniadas->execute();
+		} catch(PDOException $e) {
+	    	echo 'ERROR: ' . $e->getMessage();
+		}
+
+		return true;
+	}
 	
 	
 	
